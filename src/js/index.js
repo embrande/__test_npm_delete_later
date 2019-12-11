e93513bb708b1d7ec89346663326cd69168a9648
U-ADS\embrande <embrande@IN-STSV-22KQF12.ads.iu.edu>
2019-12-11 18:58:26 -0500

    added search and user retrieval

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,37 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-const search = new Search('pizza');
-search.getResults();
+/** Global state of the app
+	- Search object
+	- Current recipe object
+	- Shopping list object
+	- Liked recipes
+*/
+const state = {};
+
+const controlSearch = async () => {
+	// Get query from the view
+	const query = searchView.getInput();
+
+	if (query){
+		// New search object and add to state
+		state.search = new Search(query);
+
+		// Preparer UI for results
+		searchView.clearInput();
+		searchView.clearResults();
+
+		// Search for recipes
+		await state.search.getResults();
+
+		// Render results on UI
+		searchView.renderResults(state.search.result);
+	}
+}
+
+elements.searchForm.addEventListener('submit', e=>{
+	e.preventDefault();
+	controlSearch();
+});
+
